277a742e46900467e84918cfb101398048b40919
LasseKJ98 <10632102@ucn.dk>
2025-03-12 09:35:01 +0100

    Product + Equipment
    Har lavet product + equipment, fields, getters og setters, samt constructors med superclasse.

--- MP-persistence-Gruppe1/src/model/Equipment.java
+++ MP-persistence-Gruppe1/src/model/Equipment.java
@@ -1,5 +1,51 @@
 package model;
 
 public class Equipment extends Product {
+	
+	private Product product;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+public Equipment(String name, double purchasePrice,
+		double salesPrice, double rentPrice,
+		String countryOfOrigin, int minStock, String barCode) {
+	
+		super(name, purchasePrice, salesPrice, rentPrice, countryOfOrigin, minStock, barCode);
+	
+}
 
+
+
+
+
+
+
+
+
+
+public Product getProduct() {
+	return product;
+}
+
+
+
+
+
+
+
+
+
+
+public void setProduct(Product product) {
+	this.product = product;
+}
+	
+	
 }
--- MP-persistence-Gruppe1/src/model/Product.java
+++ MP-persistence-Gruppe1/src/model/Product.java
@@ -1,5 +1,104 @@
 package model;
 
-public class Product {
+public abstract class Product {
+
+	private String name;
+	private double purchasePrice;
+	private double salesPrice;
+	private double rentPrice;
+	private String countryOfOrigin;
+	private int minStock;
+	private String barCode;
+
+	
+	protected Product(String name, double purchasePrice,
+			double salesPrice, double rentPrice,
+			String countryOfOrigin, int minStock, String barCode) {
+		
+		
+	
+	this.setName(name);
+	this.setPurchasePrice(purchasePrice);
+	this.setSalesPrice(salesPrice);
+	this.setRentPrice(rentPrice);
+	this.setCountryOfOrigin(countryOfOrigin);
+	this.setMinStock(minStock);
+	this.setBarCode(barCode);
+
+
+
+
+}
+
+
+public String getName() {
+	return name;
+}
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+public double getPurchasePrice() {
+	return purchasePrice;
+}
+
+
+public void setPurchasePrice(double purchasePrice) {
+	this.purchasePrice = purchasePrice;
+}
+
+
+public double getSalesPrice() {
+	return salesPrice;
+}
+
+
+public void setSalesPrice(double salesPrice) {
+	this.salesPrice = salesPrice;
+}
+
+
+public double getRentPrice() {
+	return rentPrice;
+}
+
+
+public void setRentPrice(double rentPrice) {
+	this.rentPrice = rentPrice;
+}
+
+
+public String getCountryOfOrigin() {
+	return countryOfOrigin;
+}
+
+
+public void setCountryOfOrigin(String countryOfOrigin) {
+	this.countryOfOrigin = countryOfOrigin;
+}
+
+
+public int getMinStock() {
+	return minStock;
+}
+
+
+public void setMinStock(int minStock) {
+	this.minStock = minStock;
+}
+
+
+public String getBarCode() {
+	return barCode;
+}
+
+
+public void setBarCode(String barCode) {
+	this.barCode = barCode;
+}
+
 
 }
