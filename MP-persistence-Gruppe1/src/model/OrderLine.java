989198ead0f54b333897456afc15683a4787f4d5
hhkringel <hhkr1989@gmail.com>
2025-03-12 09:41:38 +0100

    Made OrderLine class
    Henrik

--- MP-persistence-Gruppe1/src/model/OrderLine.java
+++ MP-persistence-Gruppe1/src/model/OrderLine.java
@@ -2,4 +2,40 @@ package model;
 
 public class OrderLine {
 
+	private int quantity;
+	private double soldAtPrice;
+	private Product product;
+	
+	public OrderLine(int quantity, double soldAtPrice, Product product) {
+		
+		this.quantity = quantity;
+		this.soldAtPrice = soldAtPrice;
+		this.product = product;
+		
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getSoldAtPrice() {
+		return soldAtPrice;
+	}
+
+	public void setSoldAtPrice(double soldAtPrice) {
+		this.soldAtPrice = soldAtPrice;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
 }
