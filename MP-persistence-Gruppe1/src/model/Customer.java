a26c3880c81e267c280d96a607a87e9f54ea7856
JonasHovaldt <10632099@ucn.dk>
2025-03-12 09:05:11 +0100

    Class Customer
    Customer klassen implementeret

--- MP-persistence-Gruppe1/src/model/Customer.java
+++ MP-persistence-Gruppe1/src/model/Customer.java
@@ -1,5 +1,37 @@
 package model;
 
 public class Customer {
+	private String name;
+	private String phoneNo;
+	private String email;
+	private Address address;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
 
 }
