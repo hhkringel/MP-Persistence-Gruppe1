10853455b32c4d15531e763d0d493e3dcdce8390
hhkringel <hhkr1989@gmail.com>
2025-03-12 09:29:56 +0100

    Made Accessory class
    Henrik

--- MP-persistence-Gruppe1/src/model/Accessory.java
+++ MP-persistence-Gruppe1/src/model/Accessory.java
@@ -2,4 +2,30 @@ package model;
 
 public class Accessory extends Product {
 
+	private String type;
+	private String description;
+	
+	public Accessory(String name, double purchasePrice, double rentPrice, String countryOfOrigin, int minStock, String barcode, String type, String description) {
+		super(name, purchasePrice, rentPrice, countryOfOrigin, minStock, barcode);
+		this.type = type;
+		this.description = description;
+		
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
