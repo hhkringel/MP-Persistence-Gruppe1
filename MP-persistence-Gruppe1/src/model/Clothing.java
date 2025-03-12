4523bd056f6059e9193da7285e37756456b90825
hhkringel <hhkr1989@gmail.com>
2025-03-12 09:25:40 +0100

    Made Clothing class
    Henrik

--- MP-persistence-Gruppe1/src/model/Clothing.java
+++ MP-persistence-Gruppe1/src/model/Clothing.java
@@ -2,4 +2,29 @@ package model;
 
 public class Clothing extends Product {
 
+	private String size;
+	private String color;
+	
+	public Clothing(String name, double purchasePrice, double rentPrice, String countryOfOrigin, int minStock, String barcode, String size, String color) {
+		super(name, purchasePrice, rentPrice, countryOfOrigin, minStock, barcode);
+		this.size = size;
+		this.color = color;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
 }
