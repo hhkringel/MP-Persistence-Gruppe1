45c1c6bd694a5fcb6635b067f1dd1b4aded120f0
hhkringel <hhkr1989@gmail.com>
2025-03-12 09:34:52 +0100

    Made GunReplica class
    Henrik

--- MP-persistence-Gruppe1/src/model/GunReplica.java
+++ MP-persistence-Gruppe1/src/model/GunReplica.java
@@ -2,4 +2,30 @@ package model;
 
 public class GunReplica extends Product {
 
+	private String calibre;
+	private String material;
+	
+	public GunReplica(String name, double purchasePrice, double rentPrice, String countryOfOrigin, int minStock, String barcode, String calibre, String materiale) {
+		super(name, purchasePrice, rentPrice, countryOfOrigin, minStock, barcode);
+		this.calibre = calibre;
+		this.material = materiale;
+		
+	}
+
+	public String getCalibre() {
+		return calibre;
+	}
+
+	public void setCalibre(String calibre) {
+		this.calibre = calibre;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	
 }
