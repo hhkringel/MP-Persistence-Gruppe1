551317e2a7b3f418bd9060118e03a9fb58f1dc1e
hhkringel <hhkr1989@gmail.com>
2025-03-12 09:03:40 +0100

    Made Address classs
    Henrik

--- MP-persistence-Gruppe1/src/model/Address.java
+++ MP-persistence-Gruppe1/src/model/Address.java
@@ -2,4 +2,57 @@ package model;
 
 public class Address {
 
+	private String streetName;
+	private String zipCode;
+	private String city;
+	private String country;
+	
+	
+	public Address(String streetName, String zipCode, String city, String country) {
+		
+		this.streetName = streetName;
+		this.zipCode = zipCode;
+		this.city = city;
+		this.country = country;
+		
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 }
