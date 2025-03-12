bd32ce7bdca163e0cd05e29941dd726eb03fbe47
ItsCloudxo <10503084@ucn.dk>
2025-03-12 13:07:45 +0100

    SaleOrderDB + Interface
    Added functionality to DB object and interface
    
    Co-Authored-By: JonasHovaldt <180509236+JonasHovaldt@users.noreply.github.com>
    Co-Authored-By: Sofus Wennike <22727735+Sofus-Wennike@users.noreply.github.com>
    Co-Authored-By: hhkringel <126795885+hhkringel@users.noreply.github.com>

--- MP-persistence-Gruppe1/src/db/SaleOrderDBIF.java
+++ MP-persistence-Gruppe1/src/db/SaleOrderDBIF.java
@@ -1,5 +1,12 @@
 package db;
 
+import model.Product;
+import model.SaleOrder;
+
 public interface SaleOrderDBIF {
+	
+	SaleOrder findSaleOrderByID(int orderNo);
+	//void updateSaleOrder(); //What to update?
+	void insertSaleOrder(SaleOrder saleOrder);
 
 }
