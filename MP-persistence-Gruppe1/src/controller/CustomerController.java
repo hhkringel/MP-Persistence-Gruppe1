01477adbf453e5a985744cfb79023c53900bf4eb
JonasHovaldt <10632099@ucn.dk>
2025-03-12 13:39:38 +0100

    CustomerControllerIF og CustomerController
    CustomerController metoder ikke implementeret

--- MP-persistence-Gruppe1/src/controller/CustomerController.java
+++ MP-persistence-Gruppe1/src/controller/CustomerController.java
@@ -1,5 +1,46 @@
 package controller;
 
-public class CustomerController {
+import db.CustomerDBIF;
+import model.Address;
+import model.ClubCustomer;
+import model.Customer;
+import model.PrivateCustomer;
+
+public class CustomerController implements CustomerControllerIF {
+	private CustomerDBIF customerDB;
+	
+	public CustomerController() {
+		
+	}
+
+	@Override
+	public PrivateCustomer createPrivateCustomer(String name, String phoneNo, String email, Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClubCustomer createClubCustomer(String name, String phoneNo, String email, Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findCustomerByPhoneNo(String phoneNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- MP-persistence-Gruppe1/src/controller/CustomerControllerIF.java
+++ MP-persistence-Gruppe1/src/controller/CustomerControllerIF.java
@@ -1,8 +1,15 @@
 package controller;
 
+import model.Address;
+import model.ClubCustomer;
 import model.Customer;
+import model.PrivateCustomer;
 
 public interface CustomerControllerIF {
-	
+	public PrivateCustomer createPrivateCustomer(String name, String phoneNo, String email, Address address);
+	public ClubCustomer createClubCustomer(String name, String phoneNo, String email, Address address);
+	public Customer findCustomerByPhoneNo(String phoneNo);
+	public void updateCustomer(Customer customer);
+	public void insertCustomer(Customer customer);
 
 }
