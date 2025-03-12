2ad1d8ad8eab495ba4f9dca20f2a66f2a9a48590
hhkringel <hhkr1989@gmail.com>
2025-03-12 09:00:30 +0100

    Product class made.
    Henrik

--- MP-persistence-Gruppe1/src/model/Product.java
+++ MP-persistence-Gruppe1/src/model/Product.java
@@ -2,4 +2,70 @@ package model;
 
 public class Product {
 
+	private String name;
+	private double purchasePrice;
+	private double rentPrice;
+	private String countryOfOrigin;
+	private int minStock;
+	private String barcode;
+	
+	public Product(String name, double purchasePrice, double rentPrice, String countryOfOrigin, int minStock, String barcode) {
+		
+		this.name = name;
+		this.purchasePrice = purchasePrice;
+		this.rentPrice = rentPrice;
+		this.countryOfOrigin = countryOfOrigin;
+		this.minStock = minStock;
+		this.barcode = barcode;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public double getRentPrice() {
+		return rentPrice;
+	}
+
+	public void setRentPrice(double rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	
 }
