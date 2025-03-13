35477a5f7f778af1d77fc780a7d7b273fcac3279
hhkringel <hhkr1989@gmail.com>
2025-03-13 12:04:15 +0100

    TestCaseNo2, No3, No4
    implemented testsuite TestCaseNo2, TestCaseNo3, TestCaseNo4

--- MP-persistence-Gruppe1/src/test/TestCaseNo3ErrorInProductOrder.java
+++ MP-persistence-Gruppe1/src/test/TestCaseNo3ErrorInProductOrder.java
@@ -6,10 +6,28 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import controller.CustomerController;
+import controller.ProductController;
+import controller.SaleOrderController;
+import model.Address;
+import model.OrderLine;
+import model.PrivateCustomer;
+import model.Product;
+import model.SaleOrder;
+
 class TestCaseNo3ErrorInProductOrder {
 
+	static SaleOrderController saleOrderController;
+	static CustomerController customerController;
+	static ProductController productController;
+	static OrderLine orderline;
+	
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	public static void setUp() {
+		saleOrderController = new SaleOrderController();
+		customerController = new CustomerController();
+		productController = new ProductController();
 	}
 
 	@AfterEach
@@ -17,8 +35,24 @@ class TestCaseNo3ErrorInProductOrder {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void shouldGiveErrorOnProductOnOrder() {
+		
+		// Arrange
+		Address testAddress = new Address("Havevej", 4000, "Vejby", "Rusland");
+		PrivateCustomer testCustomer = customerController.createPrivateCustomer("Jens Vejmand", "", "givmigpenge@ofir.dk", testAddress);
+		
+		SaleOrder testSaleOrder = saleOrderController.addCustomerToOrder(testCustomer);
+		
+		Product testProduct = productController.findProductByBarcode("0");
+		
+		OrderLine testOrderLine = new OrderLine(5, testProduct);
+				
+		// Act
+		saleOrderController.addOrderLineToSaleOrder(testOrderLine);
+		
+		// Assert
+		assertEquals(testCustomer, testSaleOrder.getCustomer(), "Should be the same customer");
+		assertNull(testOrderLine.getProduct(), "Should be null");
 	}
 
 }
--- MP-persistence-Gruppe1/src/test/TestCaseNo4ErrorInCustomerOrder.java
+++ MP-persistence-Gruppe1/src/test/TestCaseNo4ErrorInCustomerOrder.java
@@ -2,23 +2,65 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.foreign.AddressLayout;
+import java.security.PrivateKey;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import controller.CustomerController;
+import controller.ProductController;
+import controller.SaleOrderController;
+import controller.SaleOrderControllerIF;
+import model.Address;
+import model.OrderLine;
+import model.PrivateCustomer;
+import model.Product;
+import model.SaleOrder;
+
 class TestCaseNo4ErrorInCustomerOrder {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+	class TestCaseNo3ErrorInProductOrder {
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+		static SaleOrderController saleOrderController;
+		static CustomerController customerController;
+		static ProductController productController;
+		static OrderLine orderline;
+		
+		
+		@BeforeEach
+		public static void setUp() {
+			saleOrderController = new SaleOrderController();
+			customerController = new CustomerController();
+			productController = new ProductController();
+		}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+		@AfterEach
+		void tearDown() throws Exception {
+		}
+
+		@Test
+		public void shouldGiveErrorOnProductOnOrder() {
+			
+			// Arrange
+			Address testAddress = new Address("Havevej", 4000, "Vejby", "Rusland");
+			PrivateCustomer testCustomer = customerController.createPrivateCustomer("Jens Vejmand", "", "givmigpenge@ofir.dk", testAddress);
+			
+			SaleOrder testSaleOrder = saleOrderController.addCustomerToOrder(testCustomer);
+			
+			Product testProduct = productController.findProductByBarcode("0");
+			
+			OrderLine testOrderLine = new OrderLine(5, testProduct);
+					
+			// Act
+			saleOrderController.addOrderLineToSaleOrder(testOrderLine);
+			
+			
+			// Assert
+			assertNull(testCustomer.getName(), "Should be null");
+			assertEquals(testProduct, testOrderLine.getProduct(), "Should be the same product");
+			
+		}
 
 }
