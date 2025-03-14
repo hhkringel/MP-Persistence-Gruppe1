59159834f2b6e433dd2494fa2eb67deb0c0c72cd
ItsCloudxo <10503084@ucn.dk>
2025-03-14 09:55:44 +0100

    Modified test cases
    Removed 3 test cases and modified one

--- MP-persistence-Gruppe1/src/test/TestCaseNo1SuccessfulOrder.java
+++ MP-persistence-Gruppe1/src/test/TestCaseNo1SuccessfulOrder.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.foreign.AddressLayout;
 import java.security.PrivateKey;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,7 +29,7 @@ class TestCaseNo1SuccessfulOrder {
 	
 	
 	@BeforeEach
-	public static void setUp() {
+	public void setUp() {
 		saleOrderController = new SaleOrderController();
 		customerController = new CustomerController();
 		productController = new ProductController();
@@ -43,20 +44,21 @@ class TestCaseNo1SuccessfulOrder {
 	public void shouldGiveSuccessfulOrder() {
 		
 		// Arrange
+		saleOrderController.createSaleOrder(LocalDate.EPOCH);
 		Address testAddress = new Address("Havevej", 4000, "Vejby", "Rusland");
-		PrivateCustomer testCustomer = customerController.createPrivateCustomer("Jens Vejmand", "88888888", "givmigpenge@ofir.dk", testAddress);
+		PrivateCustomer testCustomer = customerController.createPrivateCustomer("Jens Vejmand", "999999", "givmigpenge@ofir.dk", testAddress);
 		
-		SaleOrder testSaleOrder = saleOrderController.addCustomerToSaleOrder(testCustomer);
+		saleOrderController.addCustomerToSaleOrder(testCustomer);
 		
 		Product testProduct = productController.findProductByBarcode("123456890123");
 		
 		OrderLine testOrderLine = new OrderLine(5, testProduct);
 				
 		// Act
-		saleOrderController.addOrderLineToSaleOrder(testOrderLine);
+		saleOrderController.addOrderLineToSaleOrder("123456890123", 5);
 		
 		// Assert
-		assertEquals(testCustomer, testSaleOrder.getCustomer(), "Should be the same customer");
+		assertEquals(testCustomer, saleOrderController.getCurrentSaleOrder().getCustomer(), "Should be the same customer");
 		assertEquals(testProduct, testOrderLine.getProduct(), "Should be the same product");
 		
 	}
